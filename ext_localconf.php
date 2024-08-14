477090c2930666e8077d478e72f271042c9702ff
cyril.wolfangel@talan.com <cyril.wolfangel@talan.com>
2024-08-14 14:03:53 +0200

    Enregistrement du hook pour les statistiques NLP

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,12 @@ defined('TYPO3') or die();
     // Register the NLP analysis hook
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['semantic_suggestion']['nlpAnalysis'][] = 
         \TalanHdf\SemanticSuggestionNlp\Hooks\PageAnalysisHook::class . '->analyze';
+    
+        // Enregistrement du hook pour les statistiques NLP
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['semantic_suggestion']['nlpStatistics'][] = 
+    \TalanHdf\SemanticSuggestionNlp\Hooks\NlpStatisticsHook::class;
 
+    
     // Register backend module for NLP configuration
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'SemanticSuggestionNlp',
